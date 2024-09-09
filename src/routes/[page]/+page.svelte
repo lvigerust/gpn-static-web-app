21f78beec4c360768ababbd9b7bc20a7dacce35b
Ludvig Vigerust <l.vigerust@gmail.com>
2024-09-09 16:44:06 +0200

    reactive page

--- src/routes/[page]/+page.svelte
+++ src/routes/[page]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let data;
 
-	const { page } = data;
+	$: ({ page } = data);
 </script>
 
 <h1 class="text-5xl font-bold tracking-tight">Page {page} retrived from Azure Functions</h1>
