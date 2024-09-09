fe58ef53e5db35519892719e6c186a2a599138f2
Ludvig Vigerust <l.vigerust@gmail.com>
2024-09-09 16:40:56 +0200

    dynamic routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 </script>
 
-<slot></slot>
+<nav class="container mx-auto w-full py-8">
+	<ul class="flex gap-x-8">
+		<li><a href="/1">Page 1</a></li>
+		<li><a href="/2">Page 2</a></li>
+	</ul>
+</nav>
+
+<main class="container mx-auto">
+	<slot />
+</main>
