e67e9aaacbfb3851cfd4147ab57abefd846700d4
Ludvig Vigerust <l.vigerust@gmail.com>
2024-09-09 22:13:06 +0200

    animatioooon

---
+++ src/lib/components/ViewTransitions.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { onNavigate } from '$app/navigation'
+
+	onNavigate((navigation) => {
+		// @ts-expect-error - View Transition API is not yet typed
+		if (!document.startViewTransition) return
+
+		return new Promise((resolve) => {
+			// @ts-expect-error - View Transition API is not yet typed
+			document.startViewTransition(async () => {
+				resolve()
+				await navigation.complete
+			})
+		})
+	})
+</script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import '../app.css'
-	import { Header } from '$components'
+	import { Header, ViewTransitions } from '$components'
 </script>
 
+<ViewTransitions />
 <Header />
 
 <main class="container mx-auto py-24">
